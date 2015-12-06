1da34a27d2f3c72adf45815362f530c8710b5a53
ChrisVasShiver <c_visscher@hotmail.com>
2015-12-06 23:24:54 +0100

    method Read in Reader
    made a start

--- src/main/java/classifier/Reader.java
+++ src/main/java/classifier/Reader.java
@@ -2,14 +2,37 @@ package main.java.classifier;
 /**
  * @author C. Visscher
  */
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Reader {
-	private HashMap<?, ?> words;
 	
 	public Reader() {
 	} 
 	
+	public static void read(BufferedReader reader) throws EOFException {
+		String input;
+		String fullInput = "";
+		try {
+			while ((input = reader.readLine()) != null) {
+				fullInput = fullInput + input;		
+				}
+		} catch(IOException e) {
+			System.out.println(e.getMessage() + "Error reading file");
+		}
+		System.out.println(fullInput);
+		throw new EOFException();
+
+	}
+	
 	public void splitter() {
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader("c:/Users/Christiaan/Desktop/project/index.txt"));
+		Reader.read(reader);
+		
 	}
 }

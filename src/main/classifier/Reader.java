a440dee9af07be730c0657cf63206e84243e4980
ChrisVasShiver <c_visscher@hotmail.com>
2015-12-06 14:37:32 +0100

    Begin Reader
    eignelijk niet veel

---
+++ src/main/classifier/Reader.java
@@ -0,0 +1,15 @@
+package main.classifier;
+/**
+ * @author C. Visscher
+ */
+import java.util.HashMap;
+
+public class Reader {
+	private HashMap<?, ?> words;
+	
+	public Reader() {
+	} 
+	
+	public void splitter() {
+	}
+}
